859eb05ca3f13b55e96c8e4b8f91c8629a9b4a18
anandg112 <anandg112@gmail.com>
2017-06-06 21:16:44 -0400

    add insertMany for Mongo

---
+++ insertMany-ordered.js
@@ -0,0 +1,25 @@
+db.Movies.insertMany(
+  [
+    {
+      "_id": "tt0084726",
+      "title": "StarTrek II: The wrath of Khan",
+      "year": 1982,
+      "type": "movie"
+    },
+    {
+      "_id": "tt07966366",
+      "title": "Star Trek",
+      "year": 2009,
+      "type": "movie"
+    },
+    {
+      "_id": "tt0084726",
+      "title": "StarTrek II: The wrath of Khan",
+      "year": 1982,
+      "type": "movie"
+    }
+  ],
+  {
+    "ordered": false
+  }
+);
